0325bbab9369b1ed713c5022af2554e0b019ec4d
romulobeluco <romulocoutinhobeluco@gmail.com>
2024-10-28 23:00:48 -0300

    a

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,18 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
+      return config;
     },
+    reporter: 'cypress-mochawesome-reporter',
+    reporterOptions: {
+      reportDir: 'C:/Users/romul/OneDrive/Documentos/GitHub/S206-L1/cypress/reports',
+      overwrite: false,
+      html: true,
+      json: false,
+    },
+    
   },
 });
